18962199da72283594e85a77fa2efc4033526257
Артём Гизатуллин <Tyuiu.GizatullinAP@outlook.com>
2024-10-02 20:59:03 +0500

    Add project files.

---
+++ Tyuiu.GizatullinAP.Sprint1.Task0.V20/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.GizatullinAP.Sprint1.Task0.V20.Lib;
+
+namespace Tyuiu.GizatullinAP.Sprint1.Task0.V20_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #0 | Выполнил: Гизаутллин А. П. | ИСПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Гизатуллин Артём Павлович                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 10+9/3 и                *");
+            Console.WriteLine("* печатает результат на экране.                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДННЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*10 + 9 / 3                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate());
+            Console.ReadLine();
+        }
+    }
+}
